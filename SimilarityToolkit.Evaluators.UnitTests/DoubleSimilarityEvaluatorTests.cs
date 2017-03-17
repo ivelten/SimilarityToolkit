e552ca2441b141026b80097514f74368dc000dd4
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-03-16 22:13:39 -0300

    Implementing DoubleSimilarityEvaluator

---
+++ SimilarityToolkit.Evaluators.UnitTests/DoubleSimilarityEvaluatorTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using SimilarityToolkit.Evaluators.UnitTests.Fixtures;
+using System;
+using Xunit;
+
+namespace SimilarityToolkit.Evaluators.UnitTests
+{
+    public class DoubleSimilarityEvaluatorTests
+    {
+        [Theory]
+        [AutoNSubstituteInlineData(0.0, 0.0)]
+        [AutoNSubstituteInlineData(-1.7, 50.8)]
+        [AutoNSubstituteInlineData(0.0, 50.8843)]
+        [AutoNSubstituteInlineData(1567.12, 0.0)]
+        [AutoNSubstituteInlineData(987.0, -67.2312)]
+        public void Similarity_Test_01(double item1, double item2, DoubleSimilarityEvaluator evaluator)
+        {
+            evaluator.EvaluateDistance(item1, item2).Should().Be(Math.Abs(item1 - item2));
+        }
+    }
+}
---
+++ SimilarityToolkit.Evaluators/DoubleSimilarityEvaluator.cs
@@ -0,0 +1,13 @@
+﻿using SimilarityToolkit.Evaluators.Abstractions;
+using System;
+
+namespace SimilarityToolkit.Evaluators
+{
+    public class DoubleSimilarityEvaluator : ISimilarityEvaluator<double, double>
+    {
+        public double EvaluateDistance(double item1, double item2)
+        {
+            return Math.Abs(item1 - item2);
+        }
+    }
+}
