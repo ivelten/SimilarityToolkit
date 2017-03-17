67ead99c1826a6a1f0b393320bec2f5c92e06429
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-03-16 22:07:55 -0300

    Refactoring tests

--- SimilarityToolkit.Evaluators.UnitTests/StringSimilarityEvaluatorTests.cs
+++ SimilarityToolkit.Evaluators.UnitTests/StringSimilarityEvaluatorTests.cs
@@ -6,70 +6,70 @@ namespace SimilarityToolkit.Evaluators.UnitTests
 {
     public class StringSimilarityEvaluatorTests
     {
-        [Theory, AutoNSubstituteInlineData("abc", "abc")]
-        public void Similarity_Test_01(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_01(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(0);
+            evaluator.EvaluateDistance("abc", "abc").Should().Be(0);
         }
 
-        [Theory, AutoNSubstituteInlineData("abc", "abC")]
-        public void Similarity_Test_02(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_02(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(1);
+            evaluator.EvaluateDistance("abc", "abC").Should().Be(1);
         }
 
-        [Theory, AutoNSubstituteInlineData("abc", "abC ")]
-        public void Similarity_Test_03(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_03(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(2);
+            evaluator.EvaluateDistance("abc", "abC ").Should().Be(2);
         }
 
-        [Theory, AutoNSubstituteInlineData("", "")]
-        public void Similarity_Test_04(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_04(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(0);
+            evaluator.EvaluateDistance("", "").Should().Be(0);
         }
 
-        [Theory, AutoNSubstituteInlineData(null, "")]
-        public void Similarity_Test_05(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_05(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(0);
+            evaluator.EvaluateDistance(null, "").Should().Be(0);
         }
 
-        [Theory, AutoNSubstituteInlineData("", null)]
-        public void Similarity_Test_06(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_06(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(0);
+            evaluator.EvaluateDistance("", null).Should().Be(0);
         }
 
-        [Theory, AutoNSubstituteInlineData(null, null)]
-        public void Similarity_Test_07(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_07(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(0);
+            evaluator.EvaluateDistance(null, null).Should().Be(0);
         }
 
-        [Theory, AutoNSubstituteInlineData(null, "abc")]
-        public void Similarity_Test_08(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_08(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(3);
+            evaluator.EvaluateDistance(null, "abc").Should().Be(3);
         }
 
-        [Theory, AutoNSubstituteInlineData("abc", null)]
-        public void Similarity_Test_09(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_09(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(3);
+            evaluator.EvaluateDistance("abc", null).Should().Be(3);
         }
 
-        [Theory, AutoNSubstituteInlineData("", "abc")]
-        public void Similarity_Test_10(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_10(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(3);
+            evaluator.EvaluateDistance("", "abc").Should().Be(3);
         }
 
-        [Theory, AutoNSubstituteInlineData("abc", "")]
-        public void Similarity_Test_11(string item1, string item2, StringSimilarityEvaluator evaluator)
+        [Theory, AutoNSubstituteData]
+        public void Similarity_Test_11(StringSimilarityEvaluator evaluator)
         {
-            evaluator.EvaluateDistance(item1, item2).Should().Be(3);
+            evaluator.EvaluateDistance("abc", "").Should().Be(3);
         }
     }
 }
