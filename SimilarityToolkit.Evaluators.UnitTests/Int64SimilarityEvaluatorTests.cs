66ed873f98754534fce6624ac5b099f2675d02e5
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-03-16 22:17:38 -0300

    Extending Int64SimilarityEvaluator to handle Nullable<long>

--- SimilarityToolkit.Evaluators.UnitTests/Int64SimilarityEvaluatorTests.cs
+++ SimilarityToolkit.Evaluators.UnitTests/Int64SimilarityEvaluatorTests.cs
@@ -17,5 +17,20 @@ namespace SimilarityToolkit.Evaluators.UnitTests
         {
             evaluator.EvaluateDistance(item1, item2).Should().Be(Math.Abs(item1 - item2));
         }
+
+        [Theory]
+        [AutoNSubstituteInlineData(0, 0)]
+        [AutoNSubstituteInlineData(-1, 50)]
+        [AutoNSubstituteInlineData(0, 50)]
+        [AutoNSubstituteInlineData(1567, 0)]
+        [AutoNSubstituteInlineData(987, -67)]
+        [AutoNSubstituteInlineData(null, -67)]
+        [AutoNSubstituteInlineData(-67, null)]
+        [AutoNSubstituteInlineData(0, null)]
+        [AutoNSubstituteInlineData(null, 0)]
+        public void Similarity_Test_01(long? item1, long? item2, Int64SimilarityEvaluator evaluator)
+        {
+            evaluator.EvaluateDistance(item1, item2).Should().Be(Math.Abs(item1 ?? 0 - item2 ?? 0));
+        }
     }
 }
--- SimilarityToolkit.Evaluators/Int64SimilarityEvaluator.cs
+++ SimilarityToolkit.Evaluators/Int64SimilarityEvaluator.cs
@@ -3,8 +3,13 @@ using System;
 
 namespace SimilarityToolkit.Evaluators
 {
-    public class Int64SimilarityEvaluator : ISimilarityEvaluator<long, long>
+    public class Int64SimilarityEvaluator : ISimilarityEvaluator<long, long>, ISimilarityEvaluator<long?, long?>
     {
+        public double EvaluateDistance(long? item1, long? item2)
+        {
+            return Math.Abs(item1 ?? 0 - item2 ?? 0);
+        }
+
         public double EvaluateDistance(long item1, long item2)
         {
             return Math.Abs(item1 - item2);
