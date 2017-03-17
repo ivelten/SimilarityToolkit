d3161999e6b1f2dd34520cf58e60d55480d01e79
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-03-16 22:21:09 -0300

    Refactoring test name

--- SimilarityToolkit.Evaluators.UnitTests/Int64SimilarityEvaluatorTests.cs
+++ SimilarityToolkit.Evaluators.UnitTests/Int64SimilarityEvaluatorTests.cs
@@ -28,7 +28,7 @@ namespace SimilarityToolkit.Evaluators.UnitTests
         [AutoNSubstituteInlineData(-67, null)]
         [AutoNSubstituteInlineData(0, null)]
         [AutoNSubstituteInlineData(null, 0)]
-        public void Similarity_Test_01(long? item1, long? item2, Int64SimilarityEvaluator evaluator)
+        public void Similarity_Test_02(long? item1, long? item2, Int64SimilarityEvaluator evaluator)
         {
             evaluator.EvaluateDistance(item1, item2).Should().Be(Math.Abs(item1 ?? 0 - item2 ?? 0));
         }
