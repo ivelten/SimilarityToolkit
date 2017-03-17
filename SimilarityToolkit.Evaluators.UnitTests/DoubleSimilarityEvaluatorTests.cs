1bc2f09a5bbe55de3c6709af8b86703ee4b0d4f6
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-03-16 22:20:56 -0300

    Extending DoubleSimilarityEvaluator to handle Nullable<double>

--- SimilarityToolkit.Evaluators.UnitTests/DoubleSimilarityEvaluatorTests.cs
+++ SimilarityToolkit.Evaluators.UnitTests/DoubleSimilarityEvaluatorTests.cs
@@ -17,5 +17,20 @@ namespace SimilarityToolkit.Evaluators.UnitTests
         {
             evaluator.EvaluateDistance(item1, item2).Should().Be(Math.Abs(item1 - item2));
         }
+
+        [Theory]
+        [AutoNSubstituteInlineData(0.0, 0.0)]
+        [AutoNSubstituteInlineData(-1.7, 50.8)]
+        [AutoNSubstituteInlineData(0.0, 50.8843)]
+        [AutoNSubstituteInlineData(1567.12, 0.0)]
+        [AutoNSubstituteInlineData(987.0, -67.2312)]
+        [AutoNSubstituteInlineData(null, -67.768)]
+        [AutoNSubstituteInlineData(-67.98798, null)]
+        [AutoNSubstituteInlineData(0.0, null)]
+        [AutoNSubstituteInlineData(null, 0.0)]
+        public void Similarity_Test_02(double? item1, double? item2, DoubleSimilarityEvaluator evaluator)
+        {
+            evaluator.EvaluateDistance(item1, item2).Should().Be(Math.Abs(item1 ?? 0 - item2 ?? 0));
+        }
     }
 }
--- SimilarityToolkit.Evaluators/DoubleSimilarityEvaluator.cs
+++ SimilarityToolkit.Evaluators/DoubleSimilarityEvaluator.cs
@@ -3,8 +3,13 @@ using System;
 
 namespace SimilarityToolkit.Evaluators
 {
-    public class DoubleSimilarityEvaluator : ISimilarityEvaluator<double, double>
+    public class DoubleSimilarityEvaluator : ISimilarityEvaluator<double, double>, ISimilarityEvaluator<double?, double?>
     {
+        public double EvaluateDistance(double? item1, double? item2)
+        {
+            return Math.Abs(item1 ?? 0.0 - item2 ?? 0.0);
+        }
+
         public double EvaluateDistance(double item1, double item2)
         {
             return Math.Abs(item1 - item2);
